459d89fef7b3a80091cb12ef0d9fd765bc8735c9
Nur Kholiq Ansori <nurkholiqansori8@gmail.com>
2022-01-22 15:22:26 +0700

    Update Portofolio

--- components/Dialog.tsx
+++ components/Dialog.tsx
@@ -1,5 +1,6 @@
 import { Dialog, Transition } from '@headlessui/react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { Fragment, useState } from 'react'
 import { data } from '../pages/api/data'
 
@@ -27,6 +28,12 @@ const DialogPortofolio = ({
   }
 
   let flowData = data.experience[atDataState]
+
+  const iconSVG: any = {
+    wordpress: {
+      path: 'M21.469 6.825c.84 1.537 1.318 3.3 1.318 5.175 0 3.979-2.156 7.456-5.363 9.325l3.295-9.527c.615-1.54.82-2.771.82-3.864 0-.405-.026-.78-.07-1.11m-7.981.105c.647-.03 1.232-.105 1.232-.105.582-.075.514-.93-.067-.899 0 0-1.755.135-2.88.135-1.064 0-2.85-.15-2.85-.15-.585-.03-.661.855-.075.885 0 0 .54.061 1.125.09l1.68 4.605-2.37 7.08L5.354 6.9c.649-.03 1.234-.1 1.234-.1.585-.075.516-.93-.065-.896 0 0-1.746.138-2.874.138-.2 0-.438-.008-.69-.015C4.911 3.15 8.235 1.215 12 1.215c2.809 0 5.365 1.072 7.286 2.833-.046-.003-.091-.009-.141-.009-1.06 0-1.812.923-1.812 1.914 0 .89.513 1.643 1.06 2.531.411.72.89 1.643.89 2.977 0 .915-.354 1.994-.821 3.479l-1.075 3.585-3.9-11.61.001.014zM12 22.784c-1.059 0-2.081-.153-3.048-.437l3.237-9.406 3.315 9.087c.024.053.05.101.078.149-1.12.393-2.325.609-3.582.609M1.211 12c0-1.564.336-3.05.935-4.39L7.29 21.709C3.694 19.96 1.212 16.271 1.211 12M12 0C5.385 0 0 5.385 0 12s5.385 12 12 12 12-5.385 12-12S18.615 0 12 0',
+    },
+  }
 
   return (
     <Transition appear show={isOpen} as={Fragment}>
@@ -68,43 +75,101 @@ const DialogPortofolio = ({
                 as='h3'
                 className='text-lg font-medium leading-6 text-gray-900'
               >
-                {flowData.title}
+                {info ? flowData.title : flowData.com}
               </Dialog.Title>
               {info ? (
                 <div className='mt-2'>
                   <p className='text-sm text-gray-500'>
-                    Company: {flowData.com}
+                    Company : {flowData.com}
                   </p>
+                  <p className='text-sm text-gray-500 flex gap-1'>
+                    Website : {flowData.address}{' '}
+                    <Link href={flowData.address}>
+                      <a
+                        className='hover:text-black'
+                        title={flowData.com}
+                        target='_blank'
+                        rel='noopener noreferer'
+                      >
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          className='h-3 w-3'
+                          fill='none'
+                          viewBox='0 0 24 24'
+                          stroke='currentColor'
+                        >
+                          <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            strokeWidth={2}
+                            d='M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14'
+                          />
+                        </svg>
+                      </a>
+                    </Link>
+                  </p>
+
                   <p className='text-sm text-gray-500'>
-                    Website: {flowData.address}
+                    {' '}
+                    Job Type : {flowData.status}
                   </p>
                   <p className='text-sm text-gray-500'>
                     {' '}
                     Develop on {flowData.date}
                   </p>
-
-                  <p>{atDataState}</p>
+                  <div className='text-sm text-gray-500 flex'>
+                    Build with{' '}
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      className='h-5 w-5 ml-3'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'
+                    >
+                      <path d={iconSVG[flowData.build].path} />
+                    </svg>{' '}
+                  </div>
                 </div>
               ) : (
-                <div className='max-h-1/2'>
-                  {console.log(flowData.img === null)
-                  }
+                <div className='mt-2 max-h-72 overflow-auto scrollbar-thin scrollbar-thumb-sky-100 hover:scrollbar-thumb-sky-500 scrollbar-thumb-rounded-md'>
                   {flowData.img ? (
-                    <img src={flowData.img} />
+                    <img
+                      src={flowData.img}
+                      className='w-full h-full m-auto'
+                      alt={'Documentation of ' + flowData.com}
+                    />
                   ) : (
                     'No Image'
                   )}
                 </div>
               )}
 
-              <div className='mt-4'>
+              <div className='mt-4 grid grid-cols-2 gap-5'>
                 <button
                   type='button'
-                  className='inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500'
+                  className='px-4 py-2 text-sm font-medium text-sky-900 bg-sky-100 border border-transparent rounded-md hover:bg-sky-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-sky-500'
                   onClick={closeModal}
                 >
                   Close
                 </button>
+                {flowData.img ? (
+                  <Link href={flowData.img}>
+                    <a
+                      title='Full View'
+                      target='_blank'
+                      rel='noopener noreferer'
+                      className='w-full'
+                    >
+                      <button
+                        type='button'
+                        className='w-full inline-flex justify-center px-4 py-2 text-sm font-medium text-sky-900 bg-sky-100 border border-transparent rounded-md hover:bg-sky-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-sky-500'
+                      >
+                        Full View
+                      </button>
+                    </a>
+                  </Link>
+                ) : (
+                  ''
+                )}
               </div>
             </div>
           </Transition.Child>
--- components/Icon.tsx
+++ components/Icon.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 type Props = {
   id?: number
-  name: string | any
-  color: any
-  link: any
-  path: any
+  name: string
+  color: string
+  link: string
+  path: string
 }
 
 const Icon = ({ id, name, color, link, path }: Props) => {
@@ -16,19 +16,20 @@ const Icon = ({ id, name, color, link, path }: Props) => {
         <a title={name} target='_blank' rel='noopener noreferrer'>
           <div
             key={id}
-            className={(color ? (`bg-[${color}]`) : 'bg-sky-100') + ' p-2 m-2 w-10 h-10 flex justify-center items-center rounded-full hover:scale-95 cursor-pointer overflow-hidden mx-auto'}
+            className={`bg-[#${color}] p-2 m-2 w-10 h-10 flex justify-center items-center rounded-full hover:scale-95 cursor-pointer overflow-hidden mx-auto text-white`}
           >
             <svg
               xmlns='http://www.w3.org/2000/svg'
-              className='h-5 w-5'
-              fill='none'
+              className='h-6 w-6'
               viewBox='0 0 24 24'
+              fill='none'
               stroke='currentColor'
+              role='img'
             >
               <path
                 strokeLinecap='round'
                 strokeLinejoin='round'
-                strokeWidth={2}
+                strokeWidth={1}
                 d={path}
               />
             </svg>
