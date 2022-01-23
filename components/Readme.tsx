ef843cc5191c5243be97d5f44987218728f38a9c
Nur Kholiq Ansori <nurkholiqansori8@gmail.com>
2022-01-23 20:54:56 +0700

    Update readme

--- components/Readme.tsx
+++ components/Readme.tsx
@@ -3,12 +3,14 @@ import ReactMarkdown from 'react-markdown'
 import rehypeSanitize from 'rehype-sanitize'
 import remarkHtml from 'remark-html'
 import Link from 'next/link'
+import Image from 'next/image'
+import remarkUnwrapImages from 'remark-unwrap-images'
 
 type Props = {
   datas: any
 }
 
-const Readme = ({ datas, }: Props) => {
+const Readme = ({ datas }: Props) => {
   const datak = JSON.parse(datas)
 
   const frontMatter = Buffer.from(datak.content, 'base64').toString('utf-8')
@@ -19,24 +21,36 @@ const Readme = ({ datas, }: Props) => {
   const H2 = ({ node, ...props }: any) => (
     <h2 className='text-lg font-medium my-2' {...props} />
   )
-  const Images = ({ node, ...props }: any) => (
-    <img className='inline' alt={node.alt} {...props} />
-  )
+
   const Linked = ({ node, ...props }: any) => (
     <a className='text-sky-500 hover:text-sky-700' {...props} />
   )
+  const Paragraph = ({ node, ...props }: any) => {
+    const { children } = props
+    if (
+      children &&
+      children[0].props &&
+      children[0].props.src
+    ) {
+
+      return <div className='flex flex-wrap gap-2 my-5 justify-center' {...props} />
+    }
+
+    return <div className='my-5 inline flex-wrap' {...props} />
+  }
+
 
   return (
     <>
       <ReactMarkdown
         children={frontMatter}
-        rehypePlugins={[rehypeSanitize]}
         remarkPlugins={[remarkHtml]}
+        rehypePlugins={[rehypeSanitize]}
         components={{
           h1: H1,
           h2: H2,
-          img: Images,
           a: Linked,
+          p: Paragraph,
         }}
       />
     </>
--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    domains: ['img.shields.io'],
     formats: ['image/avif', 'image/webp'],
   },
   webpack: (config, { isServer }) => {
