c398be018369d7db206c34d39b3e1f21aee6ee5e
Nur Kholiq Ansori <nurkholiqansori8@gmail.com>
2022-01-28 22:16:18 +0700

    Header Updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,18 +11,19 @@ export const Header = ({title, url}: Props) => {
     <>
       <NextSeo
         title={`${title} | Nur Kholiq Ansori Profile`}
-        description=''
-        canonical='https://www.nurkholiqansori.me'
+        description="My Name is Nur Kholiq Ansori, I'm a Computer Science who likes Javascript Framework"
+        canonical='https://nurkholiqansori.me'
         openGraph={{
-          url: `https://www.nurkholiqansori.me${url}`,
+          url: `https://nurkholiqansori.me${url}`,
           title: `${title} | Nur Kholiq Ansori Profile`,
-          description: '',
+          description:
+            'My Name is Nur Kholiq Ansori, I\'m a Computer Science who likes Javascript Framework',
           images: [
             {
               url: '/logo.png',
               width: 800,
               height: 600,
-              alt: 'Og Image Alt',
+              alt: 'Logo Nur Kholiq Ansori',
               type: 'image/png',
             },
           ],
@@ -36,21 +37,13 @@ export const Header = ({title, url}: Props) => {
         }}
         twitter={{
           handle: '@EnjoyKudasai',
-          site: `https://www.nurkholiqansori.me${url}`,
+          site: `https://nurkholiqansori.me${url}`,
           cardType: 'summary_large_image',
         }}
         additionalMetaTags={[
           {
-            property: 'dc:creator',
-            content: 'Jane Doe',
-          },
-          {
             name: 'application-name',
             content: 'Nur Kholiq Ansori Profile',
-          },
-          {
-            httpEquiv: 'x-ua-compatible',
-            content: 'IE=edge; chrome=1',
           },
         ]}
         additionalLinkTags={[
@@ -72,12 +65,11 @@ export const Header = ({title, url}: Props) => {
       <SocialProfileJsonLd
         type='Person'
         name='Nur Kholiq Ansori'
-        url='http://www.nurkholiqansori.me'
+        url='http://nurkholiqansori.me'
         sameAs={[
-          'http://www.facebook.com/your-profile',
-          'http://instagram.com/yourProfile',
-          'http://www.linkedin.com/in/yourprofile',
-          'http://plus.google.com/your_profile',
+          'http://www.facebook.com/nurkholiq.ansori.1',
+          'http://instagram.com/nurkholiqansori',
+          'http://www.linkedin.com/in/nurkholiqansori',
         ]}
       />
     </>
