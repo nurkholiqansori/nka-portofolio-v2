b561cf06a8c4d7d6439c79db553a19f10c96bc3d
Nur Kholiq Ansori <nurkholiqansori8@gmail.com>
2022-02-16 15:51:24 +0700

    Fixed

--- pages/wp-theme.tsx
+++ pages/wp-theme.tsx
@@ -60,7 +60,7 @@ const Collections: NextPage = (props: Props) => {
                   <div className='text-gray-900 dark:text-gray-200 text-center my-5 after:content-divider'>
                     or select the category
                   </div>
-                  {data.wptheme.length} Theme
+                  <p className='text-xs italic text-right'> - Total {data.wptheme.length} Theme</p>
                 </div>
               </div>
             </div>
